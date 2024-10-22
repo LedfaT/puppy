dcaed4653d928f7654e5bb68d5d9a37d4e15cfe5
David Zaitsev <zaitsevdavid4@gmail.com>
2024-10-22 22:04:34 +0300

    third

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/LedfaT/puppy
 
 go 1.22.5
+
+require github.com/LedfaT/dog v0.0.0-20241014185353-ef916957730f // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,15 @@
 package puppy
 
+import "github.com/LedfaT/dog"
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
 }
